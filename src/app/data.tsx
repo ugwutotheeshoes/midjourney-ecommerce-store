42cfaf5b70ea58e32e50e6c89cae8f6894483785
Emmanuel Ugwu <32464178+ugwutotheeshoes@users.noreply.github.com>
2024-09-07 10:33:24 +0100

    new files

---
+++ src/app/data.tsx
@@ -0,0 +1,45 @@
+export interface ImageDataProp {
+    name: string;
+    url: string;
+    price: number;
+    transformations: string;
+}
+
+export const ImageData: ImageDataProp[] = [
+    {
+        name: "Crayon Art Illustration",
+        url: "https://res.cloudinary.com/ugwutotheeshoes/image/upload/c_scale,h_300,w_200/v1725571897/crayon%20illustration.png",
+        price: 1200,
+        transformations: "jajja"
+    },
+    {
+        name: "Miniature Farmers",
+        url: "https://res.cloudinary.com/ugwutotheeshoes/image/upload/c_scale,h_300,w_200/v1725571895/Miniature%20scenes%20of%20rice%20harvesting.png",
+        price: 2100,
+        transformations: "jajja"
+    },
+    {
+        name: "The Boy and the Toy Car",
+        url: "https://res.cloudinary.com/ugwutotheeshoes/image/upload/c_scale,h_300,w_200/v1725571890/boy%20playing%20with%20a%20toy%20car.png",
+        price: 10000,
+        transformations: "jajja"
+    },
+    {
+        name: "One Cocktail Please",
+        url: "https://res.cloudinary.com/ugwutotheeshoes/image/upload/c_scale,h_300,w_200/v1725571890/cocktail.png",
+        price: 1300,
+        transformations: "jajja"
+    },
+    {
+        name: "Travel Poster",
+        url: "https://res.cloudinary.com/ugwutotheeshoes/image/upload/c_scale,h_300,w_200/v1725571889/Travel%20Poster.png",
+        price: 200,
+        transformations: "jajja"
+    },
+    {
+        name: "High Rise Buildings",
+        url: "https://res.cloudinary.com/ugwutotheeshoes/image/upload/c_scale,h_300,w_200/v1725571888/modern%20high%20rise%20buildings.png",
+        price: 700,
+        transformations: "jajja"
+    }
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,34 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import styles from "./page.module.css";
+import { ImageData, ImageDataProp } from './data';
 
 export default function Home() {
+  const data: ImageDataProp[] = ImageData
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1>digital art store</h1>
 
         <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          {data.map((image) => {
+            return (
+                <div className={styles.primary} key={image.name}>
+                  <img
+                    className={styles.img}
+                    src={image.url}
+                    alt={image.name}
+                  />
+                  <h3>{image.name}</h3>
+                  <span>
+                    <p>${image.price}</p>
+                    <button>buy</button>
+                  </span>
+                </div>
+            )
+          })}
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
     </div>
   );
 }
